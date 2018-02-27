cca54e020de0f98f718664a388eca744e5727be5
nevengjoreski <nenoseksovski@hotmail.com>
2018-02-27 19:52:52 +0100

    c4

--- admin/sys/controllers/login_check.php
+++ admin/sys/controllers/login_check.php
@@ -1,2 +1,18 @@
 <?php
-header('location: ../../index.php');
+
+// $username = 'asd@asd.asd';
+// $password = 'pass';
+
+// if($username == $_POST['loginEmail'] && 
+// 	$password == $_POST['loginPassword']){
+// 	header('location: ../../index.php');
+// }
+
+// header('location: login_check.php');
+
+ $file_content = file_get_contents('../../test.txt');
+
+
+
+echo $file_content;
+
